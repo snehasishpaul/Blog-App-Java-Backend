00532807d2f251d5e559031de2a025dc45e8feda
snehasishpaul <weishenpov@gmail.com>
2023-06-15 20:46:38 +0530

    spring boot security implementation without WebSecurityConfigurerAdapter interface
    spring boot security implementation without WebSecurityConfigurerAdapter interface

--- src/main/java/com/snehasish/blog/config/SecurityConfig.java
+++ src/main/java/com/snehasish/blog/config/SecurityConfig.java
@@ -6,14 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,7 +31,7 @@ import com.snehasish.blog.security.JWTAuthenticationFilter;
 @EnableWebSecurity
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	public static final String[] PUBLIC_URLS = { "/api/auth/**", "/v3/api-docs", "/v2/api-docs",
 			"/swagger-resources/**", "/swagger-ui/**", "/webjars/**" };
@@ -43,31 +45,65 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTAuthenticationFilter jwtAuthenticationFilter;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	// WebSecurityConfigurerAdapter deprecated
+	/*
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * 
+	 * http.csrf().disable().authorizeHttpRequests().antMatchers(PUBLIC_URLS).
+	 * permitAll().antMatchers(HttpMethod.GET)
+	 * .permitAll().anyRequest().authenticated().and().exceptionHandling()
+	 * .authenticationEntryPoint(this.jwtAuthenticationEntryPoint).and().
+	 * sessionManagement() .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	 * 
+	 * http.addFilterBefore(this.jwtAuthenticationFilter,
+	 * UsernamePasswordAuthenticationFilter.class); }
+	 * 
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * auth.userDetailsService(this.customUserDetailService).passwordEncoder(this.
+	 * passwordEncoder()); }
+	 * 
+	 * @Bean
+	 * 
+	 * @Override public AuthenticationManager authenticationManagerBean() throws
+	 * Exception { return super.authenticationManagerBean(); }
+	 * 
+	 */
 
+	// Configuration without WebSecurityConfigurerAdapter (latest)
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeHttpRequests().antMatchers(PUBLIC_URLS).permitAll().antMatchers(HttpMethod.GET)
 				.permitAll().anyRequest().authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(this.jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+		http.authenticationProvider(daoAuthenticationProvider());
+
+		DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+
+		return defaultSecurityFilterChain;
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.customUserDetailService).passwordEncoder(this.passwordEncoder());
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(this.customUserDetailService);
+		provider.setPasswordEncoder(passwordEncoder());
+
+		return provider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
 	}
 
 	@Bean
